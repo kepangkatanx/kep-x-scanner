9c7e0eb3688ca48f70a79a4a7ec7dc6affa89882
pandudigital <kepparokipandu@gmail.com>
2026-08-05 19:11:51 +0700

    Replace success message with test display
    Updated success message display in app.js.

--- app.js
+++ app.js
@@ -103,23 +103,10 @@ function prosesAbsensi(id) {
                 hasil.className = "info success";
 
                 hasil.innerHTML = `
-                    <div class="scan-title">
-                        ✅ ABSENSI BERHASIL
-                    </div>
-
-                    <div class="scan-name">
-                        ${data.nama}
-                    </div>
-
-                    <div class="scan-group">
-                        Kelompok <strong>${data.kelompok}</strong>
-                    </div>
-
-                    <div class="scan-time">
-                        🕒 ${jam} WIB
-                    </div>
-`
-                `;
+                <div style="color:red;font-size:40px;">
+                TEST
+                </div>
+`                ;
 
             } else {
 
